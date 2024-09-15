78661e37cc233e17ba6b46337ad8a28b0ed8ee68
Adeel Abbas <adeelabbasad@gmail.com>
2024-09-15 15:36:05 +0500

    Create user-data.sh
    user data shell script for linux 2 version

---
+++ day04/user-data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Use this for your user data
+# install httpd (Linux 2 version)
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
